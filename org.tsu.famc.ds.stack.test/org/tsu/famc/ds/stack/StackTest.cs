b4d62af38c1987cc4bdf05a73975af728cdd601d
Алексей Мирютов <a.mirutov@hotmail.com>
2016-03-15 01:05:35 +0600

    init

---
+++ org.tsu.famc.ds.stack.test/org/tsu/famc/ds/stack/StackTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace org.tsu.famc.ds.stack
+{
+    class StackTest
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+
+            try
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    s.Push(i);
+                }
+                Stack s1 = new Stack(s);
+                while (true)
+                {
+                    Console.WriteLine(s1.Pop());
+                }
+            }
+            catch (EStackEmpty e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ org.tsu.famc.ds.stack/org/tsu/famc/ds/stack/Stack.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace org.tsu.famc.ds.stack
+{
+
+    public class EStackEmpty : Exception
+    {
+        public EStackEmpty()
+            : this("Stack is empty")
+        {
+        }
+
+        public EStackEmpty(String message)
+            : base(message)
+        {
+        }
+    }
+
+    internal class StackItem
+    {
+        private object data;
+        private StackItem next;
+        private StackItem prev;
+
+        public StackItem(object argData)
+        {
+            data = argData;
+        }
+
+        public object Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public StackItem Prev
+        {
+            get { return prev; }
+            set { prev = value; }
+        }
+
+        public StackItem Next
+        {
+            get { return next; }
+            set { next = value; }
+
+        }
+            
+    }
+
+    public class Stack
+    {
+        private StackItem head;
+        int count;
+
+        public Stack()
+        {
+            count = 0;
+            head = null;
+        }
+
+        public Stack(Stack stack)
+        {
+            count = stack.Count;
+            if (count == 0)
+            {
+                head = null;
+                return;
+            }
+            head = new StackItem(stack.head.Data);
+            StackItem i = stack.head.Prev;
+            StackItem next = head;
+            while (i != null)
+            {
+                StackItem tmp = new StackItem(i.Data);
+                tmp.Next = next;
+                next.Prev = tmp;
+                next = tmp;
+                i = i.Prev;
+            }
+        }
+
+        public void Push(Object obj)
+        {
+            StackItem newItem = new StackItem(obj);
+            newItem.Prev = head;
+            head = newItem;
+            count++;
+        }
+
+        public Object Pop()
+        {
+            if (head == null)
+            {
+                throw new EStackEmpty("Stack is empty");
+            }
+            object ret = head.Data;
+            head = head.Prev;
+            if (head != null)
+                head.Next = null;
+            count--;
+            return ret;
+        }
+
+        public Object Top()
+        {
+            if (head == null)
+            {
+                throw new EStackEmpty("Stack is empty");
+            }
+            return head.Data;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+    }
+}
