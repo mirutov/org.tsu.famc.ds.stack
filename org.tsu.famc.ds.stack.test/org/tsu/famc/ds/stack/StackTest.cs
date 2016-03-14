8d8e12dd724b493caeef495db3bf4f1b19f57d9c
Алексей Мирютов <a.mirutov@hotmail.com>
2016-03-15 01:22:23 +0600

    some comment was added

--- org.tsu.famc.ds.stack.test/org/tsu/famc/ds/stack/StackTest.cs
+++ org.tsu.famc.ds.stack.test/org/tsu/famc/ds/stack/StackTest.cs
@@ -8,7 +8,7 @@ namespace org.tsu.famc.ds.stack
         static void Main(string[] args)
         {
             Stack s = new Stack();
-            // some comment
+            // some comment ho-ho-ho
             try
             {
                 for (int i = 0; i < 1000; i++)
